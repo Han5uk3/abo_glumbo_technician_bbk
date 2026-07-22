d0d44285adc0fa40e554e13fe9705a2d2d093500
Han5uk3 <adnanyousufpangat@gmail.com>
2026-07-22 18:13:21 +0530

    feat: implement InvoiceService to generate localized PDF invoices for bookings

--- lib/services/invoice_service.dart
+++ lib/services/invoice_service.dart
@@ -9,7 +9,6 @@ import 'package:aboglumbo_bbk_panel/models/booking.dart';
 import 'package:intl/intl.dart';
 import 'package:aboglumbo_bbk_panel/l10n/app_localizations.dart';
 import 'package:aboglumbo_bbk_panel/models/invoice.dart';
-import 'package:http/http.dart' as http;
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:arabic_reshaper/arabic_reshaper.dart';
